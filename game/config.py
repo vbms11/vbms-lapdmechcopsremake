87ba3b73aa46ebf9d6e1f5806493c9adaa46d769
silkyfx <silkyfx@googlemail.com>
2014-10-18 20:51:56 +0000

    initial commit, just playing around with python

---
+++ game/config.py
@@ -0,0 +1,25 @@
+'''
+Created on Oct 18, 2014
+
+@author: vbms
+'''
+
+__version__ = '0.1'
+__date__ = ''
+__author__ = 'Silvester Muhlhaus <silkyfx@gmail.com>'
+
+ESCAPE = '\033'
+
+key_moveForwald = "w"
+key_moveBackwald = "s"
+key_straffLeft = ""
+key_straffRight = ""
+key_turnLeft = "a"
+key_turnRight = "d"
+
+window = 0
+game = None
+lastFrameTime = None
+
+centerMouse = True;
+mouseSensitifiy = 1.0;
