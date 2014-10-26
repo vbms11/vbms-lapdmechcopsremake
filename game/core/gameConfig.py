2c50ad083644be7fa6fc7f24bedd7d62485f760b
silkyfx <silkyfx@googlemail.com>
2014-10-26 12:36:38 +0000

    initial commit, just playing around with python

---
+++ game/core/gameConfig.py
@@ -0,0 +1,21 @@
+'''
+Created on Oct 24, 2014
+
+@author: vbms
+'''
+
+class GameConfig:
+    '''
+    classdocs
+    '''
+    
+    levelFile = None
+    teams = None
+    
+    initialPlayers = None
+    
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        pass
